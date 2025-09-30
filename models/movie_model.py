2a1675918898ffade45932543c69ff7940d6919f
arodriguez-20199620 <arodriguez-2019620@kinal.edu.gt>
2025-09-30 17:51:51 -0600

    Clase 6

--- models/movie_model.py
+++ models/movie_model.py
@@ -20,7 +20,7 @@ def get_movie_by_id(id):
         if conn: conn.close()
 
 #obtener pelilculas con filtros personalizados
-def get_movies_filter(genre=None, year_from=None, year_to=None, min_rating=None):
+def get_movies_filter(genre=None, year_from=None, year_to=None, min_rating=None, order_by=None, desc=None):
     conn = None
     cursor = None
     try:
@@ -44,7 +44,13 @@ def get_movies_filter(genre=None, year_from=None, year_to=None, min_rating=None)
         # Si vienen filtros del where
         if parts:
             query += " WHERE " + " OR ".join(parts) 
-        query += ";"
+            
+        if order_by in ["titulo", "anio", "genero", "rating", "director"]:
+            if bool(desc) == True:
+                query += f" ORDER BY {order_by} DESC"
+            else:
+                query += f" ORDER BY {order_by}"
+      
         print("Query a ejecutar:", query)
         cursor.execute(query, tuple(params))
         row = cursor.fetchall()
@@ -55,6 +61,21 @@ def get_movies_filter(genre=None, year_from=None, year_to=None, min_rating=None)
     finally:
         if cursor: cursor.close()
         if conn: conn.close()
-        
-                
-        
+
+def create_movie(titulo, director, anio, raing, genero, imagen):
+    conn = None
+    cursor = None
+    try:
+        conn = get_db()
+        cursor = conn.cursor()
+        query = "INSERT INTO peliculas (titulo, director, anio, rating, genero, imagen) VALUES (%s, %s, %s, %s, %s, %s);"
+        cursor.execute(query, (titulo, director, anio, raing, genero, imagen))
+        conn.commit()
+        last_id = cursor.lastrowid
+        return last_id
+    except mysql.connector.Error as err:
+        print("Error al crear pelicula", err)
+        return None
+    finally:
+        if cursor: cursor.close()
+        if conn: conn.close()
--- routes/movies.py
+++ routes/movies.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify, request
-from models.movie_model import get_movie_by_id, get_movies_filter
+from models.movie_model import create_movie, get_movie_by_id, get_movies_filter
 
 movies_bp = Blueprint("movies", __name__)
     
@@ -16,8 +16,31 @@ def get_movies_by_filter():
     year_from = request.args.get("year_from")
     year_to = request.args.get("year_to")
     min_rating = request.args.get("min_rating")
+    order_by = request.args.get("order_by")
+    desc = request.args.get("desc", False)
 
-    movies = get_movies_filter(genre=genre, year_from=year_from, year_to=year_to, min_rating=min_rating)
+    movies = get_movies_filter(genre=genre, year_from=year_from, year_to=year_to, min_rating=min_rating, order_by=order_by, desc=desc)
     if not movies:
         return jsonify({"error": "No se encontraron peliculas con los filtros proporcionados"}), 404
     return jsonify(movies)
+
+@movies_bp.route("/", methods=["POST"])
+def add_movie():
+    # Recibimos los datos como JSON
+    data= request.json
+    titulo= data.get("titulo")
+    director= data.get("director")
+    anio= data.get("anio")
+    rating= data.get("rating")
+    genero= data.get("genero")
+    imagen= data.get("imagen")
+    
+    if not titulo or not director or anio is None:
+        return jsonify({"error": "Faltan datos obligatorios"}), 400
+    if rating is None and rating not in (None, ""):
+        return jsonify({"error": "El rating debe ser un numero"}), 400
+    
+    new_movie_id= create_movie(titulo, director, anio, rating, genero, imagen)
+    return jsonify({"message": "Pelicula creada", "id": new_movie_id}), 201
+    
+
